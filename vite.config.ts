89772f258b1c03813d9c39d2fed24f0d76232ece
ximuli <ximuli666@163.com>
2022-09-04 12:33:00 +0800

    修改vite配置：添加autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { vars } from './src/assets/style/vars.scss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,12 @@ export default defineConfig({
         additionalData: Object.keys(vars).map(item => {
           return `${item}: ${vars[item]};`
         }).join('\n')
-      }
+      },
+    },
+    postcss: {
+      plugins: [
+        autoprefixer
+      ]
     }
   },
   server: {
